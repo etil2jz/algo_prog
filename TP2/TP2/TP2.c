931a4def32e5113dd028cccff31c1f2c1196b815
John Doe <81570777+etil2jz@users.noreply.github.com>
2021-10-07 09:25:37 +0200

    cours

---
+++ TP2/TP2/TP2.c
@@ -0,0 +1,53 @@
+// Exercice 1
+
+//#include <stdio.h>
+//int main() {
+//	int i, j = 0, tab1[10], tab2[10];
+//	for (i = 0; i < 10; i++) {
+//		printf_s("\nEntrez l'entier %i du premier tableau :\n", i+1);
+//		scanf_s("%i", &tab1[i]);
+//	}
+//	for (i = 0; i < 10; i++) {
+//		printf_s("\nEntrez l'entier %i du second tableau :\n", i + 1);
+//		scanf_s("%i", &tab2[i]);
+//	}
+//	for (i = 0; i < 10; i++) {
+//		if (tab1[i] == tab2[i]) {
+//			j++;
+//		}
+//	}
+//	if (j == 10) {
+//		printf_s("\nLes tableaux sont identiques\n");
+//	}
+//}
+
+// Exercice 2
+
+//#include <stdio.h>
+//int main() {
+//	int i;
+//	float tab[10], moyenne = 0;
+//	for (i = 0; i < 10; i++) {
+//		printf_s("\nEntrez la valeur %i :\n", i + 1);
+//		scanf_s("%f", &tab[i]);
+//	}
+//	printf_s("\nVos valeurs sont :\n");
+//	for (i = 0; i < 10; i++) {
+//		printf_s("%f ", tab[i]);
+//	}
+//	for (i = 0; i < 10; i++) {
+//		moyenne += tab[i];
+//	}
+//	moyenne /= 2;
+//	printf_s("\n\nLeur moyenne arithmetique est %f\n", moyenne);
+//}
+
+// Exercice 3
+
+#include <stdio.h>
+int main() {
+	int i, j;
+	char tab[151];
+	tab[150] = 0;
+
+}
